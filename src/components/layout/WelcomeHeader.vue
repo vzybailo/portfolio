cc578b889b5f1c2d378ee5c3bd4f42261e3595cf
Vitali Zybaila <vental@bk.ru>
2025-03-14 16:56:15 -0500

    fix style for welcome page

--- src/components/layout/WelcomeHeader.vue
+++ src/components/layout/WelcomeHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="welcome">
-        <div class="welcome__switchers flex">
+        <div class="welcome__switchers flex justify-end">
             <LangSwitcher />
             <DarkLightBtn />
         </div>
@@ -9,7 +9,7 @@
             <div class="welcome__me">
                 <div class="welcome__about">
                     <img src="" alt="" class="welcome__img">
-                    <div class="welcome__name">Vitali Zybaila</div>
+                    <div class="welcome__name">{{ $t('welcome.name') }}</div>
                     <div class="welcome__position">Frontend Developer</div>
                 </div>
                 <div class="welcome__socials"></div>
@@ -31,5 +31,19 @@ import LangSwitcher from './LangSwitcher.vue'
 <style lang="scss">
 .welcome {
     height: 100vh;
+}
+.welcome__block {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    text-align: center;
+}
+.welcome__wrapper {
+    padding: 20px;
+    border: 1px gray solid;
+}
+.welcome__switchers.flex {
+    padding: 20px 0;
 }
 </style>

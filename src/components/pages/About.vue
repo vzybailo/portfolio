f0ec99d1fed657c69cafcde4066e41fba00b8d8c
vental <vental@ventals-MBP.attlocal.net>
2025-03-23 19:47:23 -0500

    add title page

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,5 +1,21 @@
-<template>about</template>
+<template>
+  <div class="about">
+    <div class="title">
+      <h2 class="title__name">About me</h2>
+      <span class="title__subname">about</span>
+    </div>
+  </div>
+</template>
 
 <script setup></script>
 
-<style></style>
+<style>
+.title {
+  text-align: center;
+  position: relative;
+}
+.title__name {
+    color: darkslategrey;
+    font-size: 46px;
+}
+</style>

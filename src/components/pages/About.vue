303f8f4ee7031d8524ac24cc624265d644da5a84
vental <vental@ventals-MBP.attlocal.net>
2025-04-07 21:05:40 -0500

    add photo about me

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
     <PageTitle :title="$t('page.about')" />
+    <div class="about__top flex">
+      <img src="../../assets/images/about-img.jpg" alt="" class="about__img">
+      <div class="about__descr">
+        <h2 class="about__title">I'm Vitali. I'm a frontend developer.</h2>
+        <div class="about__text"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +15,9 @@
 import PageTitle from '../layout/PageTitle.vue'
 </script>
 
-<style></style>
+<style>
+.about__img {
+  width: 50%;
+  filter: opacity(0.5);
+}
+</style>

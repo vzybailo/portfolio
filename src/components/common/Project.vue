34793111635be49fe18cabeebd540b10ce41123e
Vitali Zybaila <vental@bk.ru>
2025-04-17 15:18:54 -0500

    adaptive

--- src/components/common/Project.vue
+++ src/components/common/Project.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project flex items-center max-sm:flex-col">
-    <img :src="imgUrl" alt="" class="project__img mr-6" />
-    <div class="project__block w-full">
-      <div class="project__title mb-4 text-base">{{ title }}</div>
-      <div class="project__description mb-2">{{ description }}</div>
-      <ul class="project__stack flex mb-4">
-        <li class="project__stack-item" v-for="(st, index) in stack" :key="index">#{{ st }}</li>
+  <div class="flex items-center max-sm:flex-col">
+    <img :src="imgUrl" alt="" class="project__img w-[250px] mr-6 max-sm:w-auto max-sm:mr-0 max-sm:mb-4" />
+    <div>
+      <div class="mb-4 text-base">{{ title }}</div>
+      <div class="mb-2">{{ description }}</div>
+      <ul class="flex mb-4 flex-wrap">
+        <li class="project__stack-item mr-2 max-sm:mb-2" v-for="(st, index) in stack" :key="index">#{{ st }}</li>
       </ul>
       <div class="project__btns flex">
         <a
@@ -60,13 +60,11 @@ defineProps({
 
 <style lang="scss">
 .project__img {
-  width: 250px;
   filter: grayscale(1);
 }
 .project__stack-item {
   padding: 0 5px;
   background-color: $limeHighlight;
-  margin-right: 10px;
 }
 .project__btn {
   @include btn;
